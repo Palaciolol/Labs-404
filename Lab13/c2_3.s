9c03072053f18b0d9831132eac0c62e61ae71b43
Palacio <lucaspalacioa@gmail.com>
2024-11-04 22:24:35 -0300

    nao entendi

--- Lab13/c2_3.s
+++ Lab13/c2_3.s
@@ -5,66 +5,64 @@
 
 
 fill_array_int:
-    addi sp, sp, -4
-    sw ra, 0(sp)        #coloca o ra na pilha
+    addi sp, sp, -404   #aloca espaço na pilha
+    sw ra, 400(sp)
     mv t2, sp           #coloca o endereço de sp em t2
-    addi sp, sp, -416   #aloca espaço na pilha
-    mv a0, sp
     li t0, 0            #inicia variável de laço
     li t1, 100          #inicia variável de fim de laço
     1:
         bge t0, t1, 1f  #se t0 >= t1, fim  
-        sw t0, 0(sp)    #salva t0, na pilha
+        sw t0, 0(t2)    #salva t0, na pilha
+        addi t2, t2, 4  #incrementa a stack
         addi t0, t0, 1  #incrementa t0
-        addi sp, sp, 4  #incrementa a stack
         j 1b            #continua o loop
     1:
     #tenho que chamar a mystery com o adress do array em a0
+    mv a0, sp
     jal mystery_function_int    #chama a mystery
-    mv sp, t2               #recupera o sp
-    lw ra, 0(sp)            #carrega o ra
+    lw ra, 400(sp)            #carrega o ra
+    addi sp, sp, 404
     ret                     #retorna 
 
 fill_array_short:
-    sw ra, 0(sp)        #coloca o ra na pilha
+    addi sp, sp, -204   #aloca espaço na pilha
+    sw ra, 200(sp)        #coloca o ra na pilha
     mv t2, sp           #coloca o endereço de sp em t2
-    addi sp, sp, -216   #aloca espaço na pilha
-    mv a0, sp
     li t0, 0            #inicia variável de laço
     li t1, 100          #inicia variável de fim de laço
     1:
         bge t0, t1, 1f  #se t0 >= t1, fim  
-        sw t0, 0(sp)    #salva t0, na pilha
+        sh t0, 0(t2)    #salva t0, na pilha
+        addi t2, t2, 2  #incrementa a stack
         addi t0, t0, 1  #incrementa t0
-        addi sp, sp, 2  #incrementa a stack
         j 1b            #continua o loop
     1:
     #tenho que chamar a mystery com o adress do array em a0
+    mv a0, sp
     jal mystery_function_short    #chama a mystery
-    mv sp, t2               #recupera o sp
-    lw ra, 0(sp)            #carrega o ra
+    lw ra, 200(sp)            #carrega o ra
+    addi sp, sp, 204
     ret    
 
 
 fill_array_char:
-    sw ra, 0(sp)        #coloca o ra na pilha
+    addi sp, sp, -104
+    sw ra, 100(sp)        #coloca o ra na pilha
     mv t2, sp           #coloca o endereço de sp em t2
-    addi sp, sp, -112   #aloca espaço na pilha
-    mv a0, sp
     li t0, 0            #inicia variável de laço
     li t1, 100          #inicia variável de fim de laço
     1:
         bge t0, t1, 1f  #se t0 >= t1, fim  
-        sw t0, 0(sp)    #salva t0, na pilha
+        sb t0, 0(t2)    #salva t0, na pilha
         addi t0, t0, 1  #incrementa t0
-        addi sp, sp, 1  #incrementa a stack
+        addi t2, t2, 1  #incrementa a stack
         j 1b            #continua o loop
     1:
     #tenho que chamar a mystery com o adress do array em a0
+    mv a0, sp
     jal mystery_function_char    #chama a mystery
-    mv sp, t2               #recupera o sp
-    lw ra, 0(sp)            #carrega o ra
-    addi sp, sp, 4
+    lw ra, 100(sp)            #carrega o ra
+    addi sp, sp, 104
     ret    
 
 
--- Lab13/c2_5.s
+++ Lab13/c2_5.s
@@ -6,21 +6,18 @@
 node_creation:
     addi sp, sp, -4
     sw ra, 0(sp)    #salva o ra na stack
-    addi sp, sp, -8 #aloca espaço na pilha
+    addi sp, sp, -32 #aloca espaço na pilha
     mv a0, sp       #coloca o sp no a0
-    li t1, 30       #carrega o valor 30
-    sw t1, 0(sp)    #coloca o 30 na stack
-    li t1, 25       #carrega o 25
-    addi sp, sp, 4  #incrementa a stack
-    sw t1, 0(sp)    #coloca o 25 na stack
-    li t1, 64       #carrega o 64
-    addi sp, sp, 1  #incrementa a stack
-    sw t1, 0(sp)    #coloca o 64 na stack
-    li t1, -12      #carrega o -12
-    addi sp, sp ,1  #incrementa  a stack
-    sw t1, 0(sp)    #coloca o -12 na stack
+    li t0, 30       #carrega o valor 30
+    sw t0, 0(sp)    #coloca o 30 na stack
+    li t0, 25       #carrega o 25
+    sw t0, 4(sp)    #coloca o 25 na stack
+    li t0, 64       #carrega o 64
+    sw t0, 5(sp)    #coloca o 64 na stack
+    li t0, -12      #carrega o -12
+    sw t0, 6(sp)    #coloca o -12 na stack
     jal mystery_function
-    addi sp, sp, 2
+    addi sp, sp, 32
     lw ra, 0(sp)    #recupera o ra
     addi sp, sp, 4
     ret
